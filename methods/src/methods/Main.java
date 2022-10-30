4c1c6ddff0f0425fe5cb5b18574414eca7b9603b
Gokalpdemir <115492672+Gokalpdemir@users.noreply.github.com>
2022-10-30 16:32:02 +0300

    Add files via upload

---
+++ methods/src/methods/Main.java
@@ -0,0 +1,33 @@
+package methods;
+
+public class Main {
+
+	public static void main(String[] args) {
+		sayiBulmaca();
+
+	}
+
+	public static void sayiBulmaca() {
+		int[] sayilar = new int[] { 1, 2, 5, 7, 9, 0 };
+		int aranacak = 5;
+
+		boolean varMi = false;
+		for (int sayi : sayilar) {
+			if (sayi == aranacak) {
+				varMi = true;
+				break;
+			}
+		}
+		String mesaj="";
+		if (varMi) {
+			mesajVer("sayi mevcuttur:" + aranacak);
+			mesajVer(mesaj);
+		} else {
+			mesajVer("sayi mevcut degildir:" + aranacak);
+		}
+	}
+
+	public static void mesajVer(String mesaj) {
+		System.out.println(mesaj);
+	}
+}
